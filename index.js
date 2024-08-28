e856f4d2090702ea59592732252ef86d1692175e
Saadat Yaseen <saadatyaseen309@gmail.com>
2024-08-28 20:10:01 +0500

    Added extra buttons

--- index.js
+++ index.js
@@ -12,3 +12,38 @@ function updateScreen2() {
   team2 += 1;
   screen2.textContent = team2;
 }
+
+let message = "The WINNER is ";
+let barcelonaWins = "Barcelona 🥳✨🎉🎊";
+let liverpoolWins = "Liverpool 🥳✨🎉🎊";
+let draw = "It's a draw! 🤝";
+
+let messageId = document.getElementById("message");
+let winnerId = document.getElementById("winner");
+let drawId = document.getElementById("draw");
+
+function showResult() {
+  if (team1 > team2) {
+    drawId.textContent = "";
+    messageId.textContent = message;
+    winnerId.textContent = barcelonaWins;
+  } else if (team2 > team1) {
+    drawId.textContent = "";
+    messageId.textContent = message;
+    winnerId.textContent = liverpoolWins;
+  } else {
+    messageId.textContent = "";
+    winnerId.textContent = "";
+    drawId.textContent = draw;
+  }
+}
+
+function reset() {
+  team1 = 0;
+  team2 = 0;
+  screen1.textContent = team1;
+  screen2.textContent = team2;
+  drawId.textContent = "";
+  messageId.textContent = "";
+  winnerId.textContent = "";
+}
